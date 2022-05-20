0c1a33c732db3bf78d20b0c2dded0fd410bb50f5
JefterViana <76820976+JefterViana@users.noreply.github.com>
2022-05-20 19:12:26 -0300

    master

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,30 @@
+import styles from "./Cardapio.module.scss";
+import {ReactComponent as Logo} from "assets/logo.svg";
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
+
+export default function Cardapio(){
+    const [busca, setBusca] = useState("");
+    const [filtro, setFiltro] = useState<number | null>(null)
+    return(
+    <main>
+        {/*<nav className={styles.menu}>
+            <Logo/>
+            Aluroni
+    </nav>*/}
+        <header className={styles.header}>
+            <div className={styles.header__text}>
+                Meu Cantinho Maranhense
+            </div>
+        </header>
+        <section className={styles.cardapio}>
+            <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+            <Buscador busca={busca} setBusca={setBusca} />
+            <div className={styles.cardapio__filtros}>
+                <Filtros filtro={filtro} setFiltro ={setFiltro} />
+            </div>
+        </section>
+    </main> 
+    )
+}
